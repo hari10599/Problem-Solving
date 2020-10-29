dfc284b03bec16800dcafcf13aef2b6d0706f534
Hari Krishna <hari10599@gmail.com>
2020-10-30 00:03:46 +0530

    Added rehashing

--- HashMap/HashMap.java
+++ HashMap/HashMap.java
@@ -23,13 +23,43 @@ class HashMap {
         capacity = 16;
         map = new Node[capacity];
     }
+
+    public int hashCode(int key) {
+        return key % capacity;
+    }
+    
+    
+    public void reHashing() {
+        Node[] newMap = new Node[capacity * 2];
+        for(Node n : map) {
+            Node head = n;
+            while(head != null) {
+                putHelper(newMap, head.key, head.value);
+                head = head.next;
+            }
+        }
+        map = new Node[capacity * 2];
+        for(int i = 0; i < capacity; i++) {
+            map[i] = newMap[i];
+        }
+    }
     
     /** value will always be non-negative. */
     public void put(int key, int value) {
+        if(currentCapacity == (int)loadFactor * capacity) {
+            reHashing();
+            capacity *= 2;
+        }
+        putHelper(map, key, value);
+    }
+    
+    public void putHelper(Node[] map, int key, int value) {
         Node curr = new Node(key, value);
         int index = hashCode(key);
-        if(map[index] == null) 
+        if(map[index] == null) {
             map[index] = curr;
+            currentCapacity++;
+        }
         else {
             Node temp = null;
             Node prev = null;
@@ -42,25 +72,25 @@ class HashMap {
                 prev = head;
                 head = head.next;
             }
-            if(temp == null) 
+            if(temp == null){
                 prev.next = curr;
+                currentCapacity++;
+            }
             else
                 temp.value = value;
         }
+
+
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
         int index = hashCode(key);
-        if(map[index] == null)
-            return -1;
-        else {
-            Node head = map[index];
-            while(head != null) {
-                if(head.key == key)
-                    return head.value;
-                head = head.next;
-            }
+        Node head = map[index];
+        while(head != null) {
+            if(head.key == key)
+                return head.value;
+            head = head.next;
         }
         return -1;
     }
@@ -73,8 +103,9 @@ class HashMap {
             Node prev = null;
             while(head != null) {
                 if(head.key == key) {
+                    currentCapacity--;
                     if(prev == null)
-                        map[index] = prev;    
+                        map[index] = head.next;    
                     else
                         prev.next = head.next;
                     break;
@@ -86,10 +117,7 @@ class HashMap {
         }
     }
     
-    public int hashCode(int key) {
-        return key % capacity;
-    }
-    
+
 }
 
 /**
